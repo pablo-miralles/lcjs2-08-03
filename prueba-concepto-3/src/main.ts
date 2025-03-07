85aafdb04648eb62a08d2ae98e6d9e8532c53af0
Pablo Miralles <pablo@mowomo.com>
2025-03-07 14:15:53 +0100

    fin prueba-concepto-3

--- prueba-concepto-3/src/main.ts
+++ prueba-concepto-3/src/main.ts
@@ -1,3 +1,35 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const mainDiv = document.querySelector(".cards");
+const arrayTarjetas = [
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/2.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/3.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/4.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/5.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/6.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/2.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/3.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/4.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/5.png",
+	"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/6.png",
+];
+
+const generarUI = (listado: string[], contenedor: HTMLDivElement): void => {
+	for (let i = 0; i < listado.length; i++) {
+		const div = document.createElement("div");
+		const img = document.createElement("img");
+		div.classList.add("cards__item");
+		div.setAttribute("data-indice-id", i.toString());
+		img.src = "";
+		div.appendChild(img);
+		contenedor.appendChild(div);
+	}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	if (mainDiv && mainDiv instanceof HTMLDivElement) {
+		generarUI(arrayTarjetas, mainDiv);
+	}
+});
