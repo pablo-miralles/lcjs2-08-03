d91ced4fa18d1d22cb7646a06b6f3c8bb60b2b8a
Pablo Miralles <pablo@mowomo.com>
2025-03-07 13:28:50 +0100

    fin prueba concepto 2

---
+++ prueba-concepto-2/src/main.ts
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const tarjeta = document.querySelector(".cards__item");
+
+if (tarjeta && tarjeta instanceof HTMLDivElement) {
+	tarjeta.addEventListener("click", () => {
+		const img = tarjeta.querySelector("img");
+
+		if (img && img instanceof HTMLImageElement) {
+			img.src =
+				"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png";
+		}
+
+		tarjeta.classList.add("is-selected");
+	});
+}
