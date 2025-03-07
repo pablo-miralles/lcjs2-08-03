35231a224fa00688cb064824edca4cf9b5c02cd4
Pablo Miralles <pablo@mowomo.com>
2025-03-07 14:29:42 +0100

    ajustes llamada ts en todos los archivos, y terminada prueba-concepto-4

--- old/src/main.ts
+++
@@ -1,6 +0,0 @@
-import "./style.css";
-
-import { generarPartida } from "./motor";
-import { generarUI } from "./ui";
-document.addEventListener("DOMContentLoaded", generarUI);
-document.addEventListener("DOMContentLoaded", generarPartida);
--- prueba-concepto-4/src/main.ts
+++ prueba-concepto-4/src/main.ts
@@ -1,3 +1,23 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+export const mostrarImagenYVoltearCarta = (): void => {
+	const tarjetas = document.querySelectorAll(".cards__item");
+
+	if (tarjetas && tarjetas instanceof NodeList) {
+		tarjetas.forEach((elemento) => {
+			elemento.addEventListener("click", () => {
+				const img = elemento.querySelector("img");
+				elemento.classList.add("is-selected");
+
+				if (img && img instanceof HTMLImageElement) {
+					img.src =
+						"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png";
+				}
+			});
+		});
+	}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	mostrarImagenYVoltearCarta();
+});
