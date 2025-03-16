def675e2e6915a267d356e2d8017047fd053cb43
Pablo Miralles <pablo@mowomo.com>
2025-03-16 04:31:19 +0100

    returns para evitar ifs, y jugar ifs

--- ejercicio-final/src/main.ts
+++ ejercicio-final/src/main.ts
@@ -32,85 +32,57 @@ if (btnIniciar && btnIniciar instanceof HTMLButtonElement) {
 	});
 }
 
-document.addEventListener("click", (e) => {
-	console.log("=============================");
-	console.log("ANTES DE HACER CLICK");
-	console.log("=============================");
-	console.log({
-		Cartas: tablero.cartas,
-		EstadoPartida: tablero.estadoPartida,
-		NumeroIntentos: tablero.numeroIntentos,
-		"Carta A": tablero.indiceCartaVolteadaA,
-		"Carta B": tablero.indiceCartaVolteadaB,
-	});
-	const elemento = (e.target as HTMLElement).closest(".cards__item");
-	if (elemento && elemento instanceof HTMLDivElement) {
-		let idCarta = obtenerIdCarta(elemento);
-		const sePuedeVoltear = sePuedeVoltearLaCarta(tablero, idCarta);
-		console.log("sepuedevoltear", sePuedeVoltear);
-		if (sePuedeVoltear) {
-			voltearLaCarta(tablero, idCarta);
-			mostrarImagenCarta(elemento, idCarta);
-			establecerCartaAOB(tablero, idCarta);
-			const tableroCartaA = tablero.indiceCartaVolteadaA;
-			const tableroCartaB = tablero.indiceCartaVolteadaB;
-			if (tablero.estadoPartida === "DosCartasLevantadas") {
-				if (
-					tableroCartaA !== undefined &&
-					tableroCartaB !== undefined
-				) {
-					const sonLasCartasPareja = sonPareja(
-						tablero,
-						tableroCartaA,
-						tableroCartaB
-					);
-					if (sonLasCartasPareja) {
-						parejaEncontrada(tablero, tableroCartaA, tableroCartaB);
-					} else {
-						parejaNoEncontrada(
-							tablero,
-							tableroCartaA,
-							tableroCartaB
-						);
-						if (mainDiv && mainDiv instanceof HTMLDivElement) {
-							setTimeout(() => {
-								ocultarCartasErroneas(mainDiv);
-							}, 1000);
-						}
-					}
-					if (mainDiv && mainDiv instanceof HTMLDivElement) {
-						mainDiv.setAttribute("style", "pointer-events:none");
-						setTimeout(() => {
-							prepararTableroParaSiguienteJugada(tablero);
-							mainDiv.removeAttribute("style");
-							console.log("=============================");
-							console.log("TRAS UN SEGUNDO");
-							console.log("=============================");
-							console.log({
-								Cartas: tablero.cartas,
-								EstadoPartida: tablero.estadoPartida,
-								NumeroIntentos: tablero.numeroIntentos,
-								"Carta A": tablero.indiceCartaVolteadaA,
-								"Carta B": tablero.indiceCartaVolteadaB,
-							});
-						}, 1000);
-					}
-				}
-				tablero.numeroIntentos += 1;
-				updateNumeroIntentos();
-			}
-		} else {
-			establecerMensaje("No puedes pulsar esta carta de nuevo");
-		}
+const bloquearTableroMomentaneamente = () => {
+	if (mainDiv && mainDiv instanceof HTMLDivElement) {
+		mainDiv.setAttribute("style", "pointer-events:none");
+		setTimeout(() => {
+			prepararTableroParaSiguienteJugada(tablero);
+			mainDiv.removeAttribute("style");
+		}, 1000);
 	}
-	console.log("=============================");
-	console.log("DESPUES DE HACER CLICK");
-	console.log("=============================");
-	console.log({
-		Cartas: tablero.cartas,
-		EstadoPartida: tablero.estadoPartida,
-		NumeroIntentos: tablero.numeroIntentos,
-		"Carta A": tablero.indiceCartaVolteadaA,
-		"Carta B": tablero.indiceCartaVolteadaB,
-	});
+};
+
+document.addEventListener("click", (e) => {
+	const elemento = (e.target as HTMLElement).closest(".cards__item");
+
+	if (!elemento || !(elemento instanceof HTMLDivElement)) return;
+
+	let idCarta = obtenerIdCarta(elemento);
+	const sePuedeVoltear = sePuedeVoltearLaCarta(tablero, idCarta);
+
+	if (!sePuedeVoltear) {
+		establecerMensaje("No puedes pulsar esta carta de nuevo");
+		return;
+	}
+
+	voltearLaCarta(tablero, idCarta);
+	mostrarImagenCarta(elemento, idCarta);
+	establecerCartaAOB(tablero, idCarta);
+
+	const tableroCartaA = tablero.indiceCartaVolteadaA;
+	const tableroCartaB = tablero.indiceCartaVolteadaB;
+	if (
+		tablero.estadoPartida === "DosCartasLevantadas" &&
+		tableroCartaA !== undefined &&
+		tableroCartaB !== undefined
+	) {
+		const sonLasCartasPareja = sonPareja(
+			tablero,
+			tableroCartaA,
+			tableroCartaB
+		);
+		if (sonLasCartasPareja) {
+			parejaEncontrada(tablero, tableroCartaA, tableroCartaB);
+		} else {
+			parejaNoEncontrada(tablero, tableroCartaA, tableroCartaB);
+			if (mainDiv && mainDiv instanceof HTMLDivElement) {
+				setTimeout(() => {
+					ocultarCartasErroneas(mainDiv);
+				}, 1000);
+			}
+		}
+		bloquearTableroMomentaneamente();
+		tablero.numeroIntentos += 1;
+		updateNumeroIntentos();
+	}
 });
